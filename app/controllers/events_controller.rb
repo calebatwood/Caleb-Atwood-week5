e4364750742db14e1579931a4c037dc67af81222
Caleb Atwood <calebatwood@Calebs-MacBook-Air.local>
2015-02-09 12:41:24 -0700

    add index, show and new location/event pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,23 @@
 class EventsController < ApplicationController
 
   def index
-
+    @events = Event.all
   end
 
   def new
-
+    @event = Event.new
+    @location = Location.find(params[:location_id])
   end
 
   def create
-
+    @event = Event.new(event_params)
+    @event.location_id = params[:location_id]
+    @location = Location.find(params[:location_id])
+      if @event.save
+        redirect_to location_path(@location)
+      else
+        render :new
+      end
   end
 
   def edit
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,24 @@
 class LocationsController < ApplicationController
 
   def index
-
+    @locations = Location.all
   end
 
   def new
-
+    @location = Location.new
   end
 
   def create
-
+    @location = Location.new(location_params)
+      if @location.save
+        redirect_to locations_path
+      else
+        render :new
+      end
   end
 
   def edit
-
+    @location = Location.find(params[:id])
   end
 
   def update
@@ -21,7 +26,7 @@ class LocationsController < ApplicationController
   end
 
   def show
-
+    @location = Location.find(params[:id])
   end
 
   def destroy
